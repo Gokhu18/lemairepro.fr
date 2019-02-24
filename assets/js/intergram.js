385eabc5b1727bc09ed893333532bf4a6771b2ff
Quentin Lemaire <quentin.lemaire10@free.fr>
2019-02-24 18:44:14 +0100

    Initial commit

---
+++ assets/js/intergram.js
@@ -0,0 +1,12 @@
+window.intergramId = 767382981;
+window.intergramCustomizations = {
+    closedChatAvatarUrl: 'https://www.gravatar.com/avatar/ae3ee0665731b1010ed57bd608ac213b.jpg?s=180',
+    titleClosed: 'Et si on discutais ?',
+    titleOpen: "Je vous écoute",
+    introMessage: 'Bonjour, comment puis-je vous aider ?',
+    autoResponse: 'Merci pour votre message, je vous réponds au plus vite',
+    autoNoResponse: 'Je vous réponds dans les plus brefs délais',
+    mainColor: "#2861FE",
+    alwaysUseFloatingButton: false,
+    closedStyle: 'button'
+};
